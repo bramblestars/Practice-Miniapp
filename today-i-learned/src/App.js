21b6e0c140b2ec4c1901dc605eeb508c4ad77577
bramblestars <txxcui@gmail.com>
2023-12-17 23:16:56 -0500

    separate out facts component

--- today-i-learned/src/App.js
+++ today-i-learned/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Header.js";
+import CategoryFilter from "./CategoryFilters.js";
+import Form from "./Form.js";
+import Fact from "./Fact.js";
+import FactsList from "./FactsList.js"
+import "./style.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Header />
+            <Form />
+            <main className="main">
+                <CategoryFilter />
+                <FactsList />
+            </main>
+            
+        </>
+    )
 }
 
 export default App;
