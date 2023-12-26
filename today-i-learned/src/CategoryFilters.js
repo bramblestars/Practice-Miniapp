57f21b661b7b633d1830e1b090fb7740da94515d
bramblestars <txxcui@gmail.com>
2023-12-26 01:22:36 -0500

    finish basic form functionality

--- today-i-learned/src/CategoryFilters.js
+++ today-i-learned/src/CategoryFilters.js
@@ -1,22 +1,26 @@
 import { CATEGORIES } from "./Constants.js";
 
 function CategoryFilter() {
+    let key = 0;
     return (
         <aside>
           <ul >
             <li className="category">
               <button className="btn btn-all-categories">All</button>
             </li>
-            { Object.keys(CATEGORIES).map((cat) => 
-                <li className="category">
-                    <button
-                    className="btn btn-category"
-                    style={{ backgroundColor: CATEGORIES[cat] }}
-                    >
-                    { cat }
-                    </button>
-                </li>)}
-            
+            { Object.keys(CATEGORIES).map(function (cat) {
+                    key++;
+                    return (
+                        <li className="category" key={key}>
+                            <button
+                            className="btn btn-category"
+                            style={{ backgroundColor: CATEGORIES[cat] }}
+                            >
+                            { cat }
+                            </button>
+                        </li>
+                    )  
+                })}
           </ul>
         </aside>
     );
--- today-i-learned/src/Form.js
+++ today-i-learned/src/Form.js
@@ -1,16 +1,78 @@
-function Form() {
+import { useState } from "react";
+import { CATEGORIES } from "./Constants.js";
+
+function isValidHttpUrl(str) {
+    let url;
+    try {
+        url = new URL(str);
+        console.log(url);
+    } catch (error) {
+        return false;
+    }
+    return url.protocol === "http:" || url.protocol === "https:";
+} 
+
+function isValidForm(text, source, category) {
+    return text && isValidHttpUrl(source) && category && text.length <= 200;
+}
+
+function Form({ setShowForm, setFactFn }) {
+    const [text, setText] = useState("");
+    const [source, setSource] = useState("");
+    const [category, setCategory] = useState("");
+
+    function handleSubmit(e) {
+        // Prevent browser reload
+        e.preventDefault();
+
+        if (isValidForm(text, source, category)) {
+            // Create a new fact object
+            const newFact = {
+                id: Math.round(Math.random() * 10000),
+                text: text,
+                source: source,
+                category: category,
+                votesInteresting: 0,
+                votesMindblowing: 0,
+                votesFalse: 0,
+                createdIn: new Date().getFullYear()
+            }
+
+            // Add new fact to the UI (add fact to state)
+            setFactFn((facts) => [newFact, ...facts]);
+
+            // Reset input fields
+            setText("");
+            setSource("");
+            setCategory("");
+
+            // Close the form
+            setShowForm(false);
+        }
+        
+    }
+
     return (
-        <form className="fact-form">
-        <input type="text" placeholder="Share a fact with the world..." />
-        <span>200</span>
-        <input type="text" placeholder="Trustworthy source..." />
-        <select>
+        <form className="fact-form" onSubmit={handleSubmit}>
+        <input 
+            type="text" 
+            placeholder="Share a fact with the world..." 
+            value={text}
+            onChange={(e) => setText(e.target.value)}/>
+        <span>{200 - text.length}</span>
+        <input 
+            type="text" 
+            placeholder="Trustworthy source..." 
+            value={source}
+            onChange={(e) => setSource(e.target.value)}/>
+        <select
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}>
           <option value="">Choose category:</option>
-          <option value="technology">Technology</option>
-          <option value="science">Science</option>
-          <option value="finance">Finance</option>
+          { Object.keys(CATEGORIES).map((cat) => 
+            <option value={cat}>{cat[0].toUpperCase() + cat.slice(1)}</option>)}
         </select>
-        <button className="btn btn-large">Post</button>
+        <button className="btn btn-large" onClick={handleSubmit}>Post</button>
       </form>
     );
 }
